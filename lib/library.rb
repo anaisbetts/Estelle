911c5cb03cea01c3673f321a5af2779ee227fb05
Paul Betts <paul@paulbetts.org>
2007-11-23 02:05:34 -0800

    Wrap md5sum in a try/catch

--- lib/library.rb
+++ lib/library.rb
@@ -75,9 +75,14 @@ class MusicLibrary < Logger::Application
 			
 			# First, see if we've already scanned this file before (save some time
 			# and only read the first 256K)
-			md5sum = Digest::MD5.hexdigest(File.open(current, 'r') {|x| x.read(0x40000)})
-			if (@tag_info[current] = @md5_index[md5sum])
-				#log DEBUG, "Cache hit!"
+			begin	
+				md5sum = Digest::MD5.hexdigest(File.open(current, 'r') {|x| x.read(0x40000)})
+				if (@tag_info[current] = @md5_index[md5sum])
+					#log DEBUG, "Cache hit!"
+					next
+				end
+			rescue
+				@tag_info.delete current
 				next
 			end
 
